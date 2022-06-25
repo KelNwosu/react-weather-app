3b70e67dab9662d34c94a8705334a22a19bf9477
Kelechi Nwosu <kjpn00@gmail.com>
2022-06-25 00:47:20 -0600

    init commit

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'ef0bcccca0msha4365a2bf536068p1903d2jsn3f6ef814edb7',
+        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    }
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
+
+export const WEATHER_API_KEY = 'c420d4a0556ee7fb3942f9f4501d91ca';
+
